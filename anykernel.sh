ac45c92f674904bdac431f2ef101931680b44167
Himemorii <himemori@mail.com>
2022-07-01 20:53:30 +0000

    Ak3: Rebrand to Roulette
    Signed-off-by: Himemorii <himemori@mail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Roulette-Kernel by Himemori  t.me/Himemori
 do.devicecheck=1
 do.modules=0
 do.systemless=1
