ca52e6e29225a39cd74c06ab74dedda86cea89aa
itseunbin <131173080+xeunBin@users.noreply.github.com>
2024-09-27 19:26:53 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -10,9 +10,8 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=fire
-device.name2=
-device.name3+
+device.name1=selene
+device.name2=selenes
 supported.versions=
 supported.patchlevels=
 '; } # end properties
